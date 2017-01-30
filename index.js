99dbcb4840234bad24a5a69694619b0d38943043
Dennis Seidel <mail@dennisseidel.de>
2017-01-30 11:51:49 +0100

    feat: schedule appointment

--- index.js
+++ index.js
@@ -15,6 +15,21 @@ app.get('/customers', (req, res) => {
   console.log(res);
 });
 
+app.get('/customers/:id', (req, res) => {
+  const customerId = req.params.id;
+  Customer.findById(customerId)
+  .then((customer) => {
+    if(!customer) {
+      return res.status(404).send();
+    }
+    return res.send({ customer });
+  })
+  .catch((error) => {
+    console.log(error);
+    return res.status(400).send();
+  }) 
+});
+
 // Operation to create customers
 app.post('/customers', (req, res) => {
   const customer = new Customer(req.body);
